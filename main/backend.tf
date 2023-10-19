543c963d85c4bb0558bcbc1428fce3a687385651
Allan Rodriques <allanrodriques2001@gmail.com>
2023-10-19 17:17:22 +0530

    updated

--- main/backend.tf
+++ main/backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
     bucket = "eksterrabuckett"
-    key    = "backend/ToDo-App.tfstate"
+    key    = "backend/Netflix-Clone.tfstate"
     region = "us-east-1"
     dynamodb_table = "dynamodb_table_Allan"
   }
